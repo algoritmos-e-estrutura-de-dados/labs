85f0f495e71bed23ef9eee7032ea8aef007ab209
Mario H. Adaniya <mhadaniya@gmail.com>
2020-08-10 20:42:00 -0300

    :pencil: Exercícios para avaliar workflow com ferramenta

---
+++ 02-basics/src/main/java/src/Exercicio01.java
@@ -0,0 +1,22 @@
+package src;
+
+/**
+ * Escreva uma função que recebe dois valores valores inteiros 
+ * e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo 
+ * o seu resultado na variável X. A função deve retornar o valor da soma.
+ * 
+ * Exemplos:
+ * 
+ * > 10 9
+ * > 19
+ * 
+ * > -10 4
+ * > 6
+ * 
+ * Baseado em: https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+ */
+public class Exercicio01 {
+    public int solve(int a, int b){
+        return 0;
+    }
+}
---
+++ 02-basics/src/main/java/src/Exercicio02.java
@@ -0,0 +1,21 @@
+package src;
+
+/**
+ * Faça uma função de nome `solve` que receba 2 valores de ponto flutuante A e B, 
+ * que correspondem a 2 notas de um aluno. A seguir, calcule a média 
+ * do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 
+ * (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 
+ * até 10.0, sempre com uma casa decimal.
+ * 
+ * Exemplo de valores de entrada da função e a saída:
+ * > 5.0 7.1
+ * > 6.43182
+ * 
+ * > 0.0 7.1
+ * > 4.84091
+ * 
+ * Baseado em: https://www.urionlinejudge.com.br/judge/pt/problems/view/1005
+ */
+public class Exercicio02 {
+    
+}
